d259adb880c6212b5d06dd73d00df51d6e1ece18
TheDanieLSh <95643914+TheDanieLSh@users.noreply.github.com>
2024-03-07 12:20:49 +0300

    Ex0

---
+++ Ex0/index.js
@@ -0,0 +1,15 @@
+const axios = require('axios');
+const fs = require('fs');
+axios
+.get('https://jsonplaceholder.typicode.com/photos')
+.then(response => {
+    const images = response.data.map(item => (
+        item.url
+    ))
+    const thumbnails = response.data.map(item => (
+        item.thumbnailUrl
+    ))
+    console.log(images);
+    fs.writeFileSync('Ex0/images.json', JSON.stringify(images), {encoding: 'utf8', flag: 'w'});
+    fs.writeFileSync('Ex0/thumbnails.json', JSON.stringify(thumbnails), {encoding: 'utf8', flag: 'w'});
+})
