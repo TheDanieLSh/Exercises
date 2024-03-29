9734a5130da9e34df903eb4758480ff87f51a152
Danila Shuvaev <dshuvaev@art3d.ru>
2024-03-29 17:32:51 +0300

    f5

---
+++ piton/flights_parser.py
@@ -0,0 +1,22 @@
+import json
+
+with open('./flights.json', 'r', encoding='UTF-8') as file:
+    flightsJson = json.load(file)
+
+flights = flightsJson['result']['flights']
+
+result = {}
+
+for flight in flights:
+    legs = flight['flight']['legs']
+    for leg in legs:
+        segments = leg['segments']
+        for segment in segments:
+            result['depAir'] = segment['departureAirport']['caption']
+            result['depCity'] = segment['departureCity']['caption']
+            result['arrAir'] = segment['arrivalAirport']['caption']
+            result['arrCity'] = segment['arrivalCity']['caption']
+
+print(json.dumps(result, indent=1))
+
+# print(flights[0]['flight']['legs'][0]['segments'][0]['arrivalCity'])
--- test_server/websocketServer.js
+++ test_server/websocketServer.js
@@ -1,1 +1,33 @@
- io = require('socket.io')
+const io = require('socket.io');
+const express = require('express');
+
+const app = express();
+
+const server = app.listen(3000, 'localhost', () => {
+    console.log('Server successfully started on localhost:3000');
+})
+
+const socket = io(server);
+
+socket.on('connection', sock => {
+    console.log('A Client connected');
+
+    console.log(sock);
+
+    sock.on('request', data => {
+        console.log('Request received:', data);
+
+        socket.emit('4all', 'some request');
+
+        switch (data) {
+            case 'da':
+                sock.emit('response', 'pizda');
+                break;
+            case 'net':
+                sock.emit('response', 'suda net');
+                break;
+            default:
+                sock.emit('response', 'huh?')
+        }
+    })
+})
